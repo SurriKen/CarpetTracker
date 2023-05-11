845d0d5d43a1555f348818353942386232a02476
deny <667766yden@gmail.com>
2023-05-11 08:11:34 +0300

    -

--- test2.py
+++ test2.py
@@ -14,6 +14,7 @@ class Tracker:
 
     @staticmethod
     def remove_perimeter_boxes(box: list, origin_shape: tuple):
+        # print(box)
         x_min = int(origin_shape[1] * IMAGE_IRRELEVANT_SPACE_PERCENT)
         x_max = int(origin_shape[1] - origin_shape[1] * IMAGE_IRRELEVANT_SPACE_PERCENT)
         y_min = int(origin_shape[0] * IMAGE_IRRELEVANT_SPACE_PERCENT)
@@ -67,8 +68,6 @@ class Tracker:
 
     @staticmethod
     def track_box(boxes, track_dict, working_idxs, mode='coords_1'):
-        track_id = []
-
         boxes_for_track = {}
         for i, b in enumerate(boxes):
             boxes_for_track[i] = b
@@ -76,8 +75,6 @@ class Tracker:
         cur_boxes = {}
         for i in working_idxs:
             cur_boxes[i] = track_dict[i][mode][-1]
-        # xxx = {1: (0, 2), 2: (7, 0), 3: (7, 8), 4: (4, 4)}
-        # yyy = {'a': (6, 3), 'b': (6, 2), 'c': (5, 5)}
 
         distances = []
         for cur_b in cur_boxes.keys():
@@ -105,29 +102,41 @@ class Tracker:
                 distances.pop(dk)
 
         unused_boxes = [boxes_for_track.get(i) for i in unused_boxes_keys]
-        # print(f'pairs={pairs}; unused xk={xk}, unused yk={yk}')
         return tracked_boxes, unused_id_keys, unused_boxes
-
 
     def sequience_update(self, frame_id, bb1, bb2):
 
         # if carpet track is started
-        if bb1 or bb2 and self.empty_seq and frame_id - self.empty_seq[-1] >= MIN_OBJ_SEQUENCE:
+        if (bb1 or bb2) and self.empty_seq and (frame_id - self.empty_seq[-1] >= MIN_OBJ_SEQUENCE):
+            print(frame_id, self.empty_seq[-1], MIN_OBJ_SEQUENCE)
             self.carpet_seq.append(frame_id)
             self.empty_seq = []
-        elif bb1 or bb2 and self.empty_seq and frame_id - self.empty_seq[-1] < MIN_OBJ_SEQUENCE:
+            empty = False
+            carpet = True
+        elif (bb1 or bb2) and self.empty_seq and (frame_id - self.empty_seq[-1] < MIN_OBJ_SEQUENCE):
             self.carpet_seq.append(frame_id)
+            empty = True
+            carpet = True
         elif bb1 or bb2 and not self.empty_seq:
             self.carpet_seq.append(frame_id)
+            empty = False
+            carpet = True
 
         # if carpet track is ended
         elif not bb1 and not bb2 and self.carpet_seq and frame_id - self.carpet_seq[-1] >= MIN_OBJ_SEQUENCE:
             self.empty_seq.append(frame_id)
             self.carpet_seq = []
+            empty = True
+            carpet = False
         elif not bb1 and not bb2 and self.carpet_seq and frame_id - self.carpet_seq[-1] < MIN_OBJ_SEQUENCE:
             self.empty_seq.append(frame_id)
+            empty = True
+            carpet = True
         else:
             self.empty_seq.append(frame_id)
+            empty = True
+            carpet = False
+        return empty, carpet
 
     def process(self, frame_id, predict_camera_1, predict_camera_2, remove_perimeter_boxes=None):
         if remove_perimeter_boxes is None:
@@ -136,16 +145,17 @@ class Tracker:
         origin_shape_1, origin_shape_2 = (1080, 1920), (360, 640)
 
         # Remove perimeter irrelevant boxes if condition is True
-        bb1 = Tracker.remove_perimeter_boxes(bb1, origin_shape_1) if bb1 and remove_perimeter_boxes[0] else bb1
-        bb2 = Tracker.remove_perimeter_boxes(bb2, origin_shape_2) if bb2 and remove_perimeter_boxes[1] else bb2
-        self.sequience_update(frame_id, bb1, bb2)
+        # print(bb1)
+        bb1 = [Tracker.remove_perimeter_boxes(bb, origin_shape_1) for bb in bb1] if bb1 and remove_perimeter_boxes[0] else bb1
+        bb2 = [Tracker.remove_perimeter_boxes(bb, origin_shape_2) for bb in bb2] if bb2 and remove_perimeter_boxes[1] else bb2
+        empty, carpet = self.sequience_update(frame_id, bb1, bb2)
 
         # Remove dead boxes
 
         # Identify box id and fill track dictionary
         if bb1 or bb2 and not self.working_ids:
             max_id = max(list(self.tracker_dict.keys())) if self.tracker_dict else 0
-            idx = list(range(max_id+1, max([len(bb1), len(bb2)])+max_id+1))
+            idx = list(range(max_id + 1, max([len(bb1), len(bb2)]) + max_id + 1))
             self.working_ids.extend(idx)
             for i in idx:
                 self.tracker_dict = Tracker.update_id(
@@ -158,7 +168,7 @@ class Tracker:
                 )
         elif bb1 or bb2:
             max_id = max(list(self.tracker_dict.keys())) if self.tracker_dict else 0
-            idx = list(range(max_id+1, max([len(bb1), len(bb2)])+max_id+1))
+            idx = list(range(max_id + 1, max([len(bb1), len(bb2)]) + max_id + 1))
             self.working_ids.extend(idx)
             for i in idx:
                 self.tracker_dict = Tracker.update_id(
@@ -186,67 +196,16 @@ class Tracker:
 
 
 if __name__ == "__main__":
-    # tr = Tracker()
-    # for i in range(len(tracker_1_coord)):
-    #     tr.process(
-    #         frame_id=i,
-    #         predict_camera_1=tracker_1_coord[i],
-    #         predict_camera_2=tracker_2_coord[i],
-    #     )
-    #     es = tr.empty_seq[-MIN_OBJ_SEQUENCE:] if len(tr.empty_seq) >= MIN_OBJ_SEQUENCE else tr.empty_seq
-    #     cs = tr.carpet_seq[-MIN_OBJ_SEQUENCE:] if len(tr.carpet_seq) >= MIN_OBJ_SEQUENCE else tr.carpet_seq
-    #     print(i, 'empty_seq=', es, 'carpet_seq=', cs, 'predict=', tracker_1_coord[i], tracker_2_coord[i])
-    #     if i ==500:
-    #         break
-
-    xxx = {1: (0,2), 2: (7,0), 3: (7,8), 4: (4,4)}
-    yyy = {'a': (6,3), 'b': (6,2), 'c': (5,5)}
-
-    distribution_distances = {}
-    jjj = []
-    for x in xxx.keys():
-        # distribution_distances[x] = {}
-        # min_d = 1000000
-        # min_y = None
-        for y in yyy.keys():
-            d = ((xxx[x][0] - yyy[y][0])**2 + (xxx[x][1] - yyy[y][1])**2) ** 0.5
-            print(x, y , d)
-            # if d < min_d:
-            #     min_d = d
-            #     min_y = y
-            distribution_distances[(x, y)] = d
-            jjj.append((d, (x, y)))
-
-    print()
-    print(distribution_distances)
-    # print(distribution_y)
-    print()
-    xk = list(xxx.keys())
-    yk = list(yyy.keys())
-    # while distribution_distances:
-    pairs = []
-    n = min([len(xk), len(yk)])
-    while jjj:
-        # jjj = []
-        # for k, v in distribution_distances.items():
-        #     jjj.append((v, k))
-        # jjj = jjj.sort(reverse=True)
-        closest_pair = sorted(jjj)[0]
-        pairs.append(closest_pair[1])
-        print(closest_pair)
-        xk.pop(xk.index(closest_pair[1][0]))
-        yk.pop(yk.index(closest_pair[1][1]))
-        del_keys = []
-        for i, key in enumerate(jjj):
-            if closest_pair[1][0] in key[1] or closest_pair[1][1] in key[1]:
-                # distribution_distances.pop(key)
-                del_keys.append(i)
-        del_keys = sorted(del_keys, reverse=True)
-
-        # print('del key', del_keys)
-
-        for dk in del_keys:
-            jjj.pop(dk)
-        n -= 1
-
-    print(f'pairs={pairs}; unused xk={xk}, unused yk={yk}')
+    tr = Tracker()
+    for i in range(len(tracker_1_coord)):
+        tr.process(
+            frame_id=i,
+            predict_camera_1=tracker_1_coord[i],
+            predict_camera_2=tracker_2_coord[i],
+        )
+        if i > 1500:
+            es = tr.empty_seq[-MIN_OBJ_SEQUENCE:] if len(tr.empty_seq) >= MIN_OBJ_SEQUENCE else tr.empty_seq
+            cs = tr.carpet_seq[-MIN_OBJ_SEQUENCE:] if len(tr.carpet_seq) >= MIN_OBJ_SEQUENCE else tr.carpet_seq
+            print(i, 'empty_seq=', es, 'carpet_seq=', cs, 'predict=', tracker_1_coord[i], tracker_2_coord[i])
+        if i == 2000:
+            break
